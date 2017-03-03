a150cfc2dc14cc2d658b79e3d6c4fc97b022f520
Will Smidlein <will@willsmidlein.com>
2017-03-03 17:54:00 -0500

    List all users, prepping for an admin dashboard

---
+++ api/controllers/AdminController.js
@@ -0,0 +1,29 @@
+/**
+ * AdminController
+ *
+ * @description :: Server-side logic for managing admins
+ * @help        :: See http://sailsjs.org/#!/documentation/concepts/Controllers
+ */
+
+module.exports = {
+
+  // Get app users (pretty self-explanatory, eh?)
+  get_users: function(req, res){
+
+    User.find().exec(function(err, users){
+
+      var results = _(users).omit([
+        'password', // Don't send back the user's password, even if though it's encrypted
+      ]).values().value()
+
+      res.json({
+        'sucess': (err == null),
+        'results': results
+      })
+
+    })
+
+  }
+
+};
+
--- api/controllers/UserController.js
+++ api/controllers/UserController.js
@@ -10,9 +10,12 @@ module.exports = {
 	// Endpoint for full user registration
 	signup: function(req, res){
 
+		var valid = true
+
 		var ksu_id = req.param('ksu_id', false)
 
 		if(ksu_id == false || String(ksu_id).length != 9 || isNaN(ksu_id)){ // Super duper basic KSU ID validation
+			valid = false
 			res.badRequest({ 'success': false, 'message': 'Invalid KSU ID' })
 		}
 
@@ -20,12 +23,14 @@ module.exports = {
 		var last_name = req.param('first_name', false)
 
 		if(first_name == false || last_name == false){ // Make sure we're given first and last name (since the model doesn't directly validate this)
+			valid = false
 			res.badRequest({ 'success': false, 'message': 'Invalid name' })
 		}
 
 		var ksu_email = req.param('ksu_email', false)
 
 		if(ksu_email == false){ // Make sure we're given a KSU email (since the model doesn't directly validate this)
+			valid = false
 			res.badRequest({ 'success': false, 'message': 'Invalid KSU email' })
 		}
 
@@ -34,6 +39,7 @@ module.exports = {
 		var password = req.param('password', false)
 
 		if(password == false || password.length <= 6 || password.length >= 50){ // Make sure we're given a password (since the model doesn't directly validate this) and do some super basic length checks
+			valid = false
 			res.badRequest({ 'success': false, 'message': 'Invalid password' })
 		}
 
@@ -41,37 +47,41 @@ module.exports = {
 
 		// TODO: Figure out if we want people to write their bios when they first sign up (gut says no because it would take a while)
 
-		var today = new Date()
+		if(valid){
 
-		User.findOrCreate(
-			{ ksu_id: String(ksu_id) },
-			{
-				ksu_id: String(ksu_id),
-				account_type: 0, // We will update to 1 in the callback
-				joined_hacksu: today,
-				last_attended: null,
-				meetings_attended_count: 0,
-				meetings_attended: [],
-				hackathons_attended_count: 0,
-				hackathons_attended: []
-			}
-		).exec(function(err, user){
+			var today = new Date()
 
-			user.account_type = 1
-			user.first_name = first_name
-			user.last_name = last_name
-			user.ksu_email = ksu_email
-			user.email = email
-			user.new_password = password
-			user.last_seen = today
+			User.findOrCreate(
+				{ ksu_id: String(ksu_id) },
+				{
+					ksu_id: String(ksu_id),
+					account_type: 0, // We will update to 1 in the callback
+					joined_hacksu: today,
+					last_attended: null,
+					meetings_attended_count: 0,
+					meetings_attended: [],
+					hackathons_attended_count: 0,
+					hackathons_attended: []
+				}
+			).exec(function(err, user){
 
-			user.save(function(err){
-				res.json({
-					'success': (err == null)
+				user.account_type = 1
+				user.first_name = first_name
+				user.last_name = last_name
+				user.ksu_email = ksu_email
+				user.email = email
+				user.new_password = password
+				user.last_seen = today
+
+				user.save(function(err){
+					res.json({
+						'success': (err == null)
+					})
 				})
+
 			})
 
-		})
+		}
 
 	}
 
